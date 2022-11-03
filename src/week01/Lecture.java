6bd38e6cd14d371099e766804db603a6e3dc16f8
KristinaMacias <maciaskristina@gmail.com>
2022-11-02 20:58:21 -0700

    week1 notes

---
+++ src/week01/Lecture.java
@@ -0,0 +1,34 @@
+package week01;
+
+public class Lecture {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String firstName = "Kristina";
+		
+		System.out.println(firstName);
+		
+		String lastName = "Macias";
+		
+		System.out.println(lastName);
+		
+		String fullName = firstName + ' ' + lastName;
+		
+		System.out.println(fullName);
+		
+		String city = "Long Beach";
+		String state = "California";
+		
+		System.out.println("\nMs. " + fullName + " lives in " + city + ", " + state);
+		
+		int firstNumber = 10;
+		int secondNumber = 12;
+		
+		System.out.println(firstNumber == secondNumber);
+
+
+
+	}
+
+}
---
+++ src/week02/Lab.java
@@ -0,0 +1,10 @@
+package week02;
+
+public class Lab {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
