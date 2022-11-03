0f25547280300ca4dd3a2aba151b88e6fd390122
KristinaMacias <maciaskristina@gmail.com>
2022-11-02 23:00:55 -0700

    week2 loops

--- src/week02/Videos.java
+++ src/week02/Videos.java
@@ -14,7 +14,42 @@ public class Videos {
 		System.out.println(true || false);//true
 		System.out.println(true && false);//false
 		
+		//loops
+	
+		//for loop
+		for(int i = 1; i <= 10; i++) {
+			//System.out.println(i);
+			System.out.println("scoop " + i + " cups of flower.");
+		}
+		
+		for(int i = 0; i < 10; i+=2) {
+			System.out.println(i);
+		}
+		
+		//while loop - use when i have less knowledge of how many iterations are needed
+		int i = 0;
+		while(i < 10) {
+			System.out.println(i);
+			i++;
+		}
 
+		int x = 20;
+		while(x > 0) {
+			System.out.println(x);
+			x--;
+		}
+		
+		//do while loop - prints at least once
+		int y = 11; //will print 11 and exit loop because do while guarantees at least one loop
+		do {
+			System.out.println(y);
+			y++;
+		} while(y < 50);
+		
+		int index = 60;
+		do {
+			System.out.println(index);
+			index--;
+		} while (index > 0);
 	}
-
 }
