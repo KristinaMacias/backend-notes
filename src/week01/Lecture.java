38a23c9eef16d71607613faae45fc4a31f8c2c14
KristinaMacias <maciaskristina@gmail.com>
2022-11-07 14:43:02 -0800

    more week2 stuff

--- src/week01/Lecture.java
+++ src/week01/Lecture.java
@@ -10,6 +10,7 @@ public class Lecture {
 		System.out.println(firstName);
 		
 		String lastName = "Macias";
+
 		
 		System.out.println(lastName);
 		
@@ -25,9 +26,65 @@ public class Lecture {
 		int firstNumber = 10;
 		int secondNumber = 12;
 		
+		
+		//boolean operators
 		System.out.println(firstNumber == secondNumber);
-
-
+		System.out.println(city == state);
+		
+		//conditional statements (if else if)
+		if (firstName == "Kristina" && lastName == "Macias") {
+			System.out.println("Hello, " + fullName);
+		} else {
+			System.out.println("What is your full name?");
+		}
+		
+		int age = 15;
+		
+		if(age >= 16) {
+			System.out.println("You are " + age + " years old. You can get your license.");
+		} else {
+			System.out.println("Please wait " + (16 - age) + " year(s) to get your license.");
+		}
+		
+		
+		//with else if examples
+		int temperature = 80;
+		
+		if (temperature <= 40) {
+			System.out.println("The temperature is " + temperature + ", so I will wear a heavy coat.");
+		} else if (temperature >= 60 && temperature <= 79) {
+			System.out.println("The temperature is " + temperature + ", so I will wear a light sweater.");
+		} else {
+			System.out.println("The temperature is " + temperature + ", so I will wear shorts and a tshirt.");
+		}
+		
+		//conditional statements - switch
+		char grade = 'a';
+		
+		switch (grade) {
+			case 'A':
+				System.out.println("Scored at least 90%");
+			case 'a':
+				System.out.println("Scored at least 90%");
+				break;
+			case 'B':
+				System.out.println("Scored at least 80%");
+				break;
+			case 'C':
+				System.out.println("Scored at least 70%");
+				break;
+			case 'D':
+				System.out.println("Scored at least 60%");
+				break;
+			case 'F':
+				System.out.println("Did not meet requirements");
+				
+		
+		}
+		
+		//loops 
+		
+		int
 
 	}
 
--- src/week02/Videos.java
+++ src/week02/Videos.java
@@ -5,6 +5,83 @@ public class Videos {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+String firstName = "Kristina";
+		
+		System.out.println(firstName);
+		
+		String lastName = "Macias";
+
+		
+		System.out.println(lastName);
+		
+		String fullName = firstName + ' ' + lastName;
+		
+		System.out.println(fullName);
+		
+		String city = "Long Beach";
+		String state = "California";
+		
+		System.out.println("\nMs. " + fullName + " lives in " + city + ", " + state);
+		
+		int firstNumber = 10;
+		int secondNumber = 12;
+		
+		
+		//boolean operators
+		System.out.println(firstNumber == secondNumber);
+		System.out.println(city == state);
+		
+		//conditional statements (if else if)
+		if (firstName == "Kristina" && lastName == "Macias") {
+			System.out.println("Hello, " + fullName);
+		} else {
+			System.out.println("What is your full name?");
+		}
+		
+		int age = 15;
+		
+		if(age >= 16) {
+			System.out.println("You are " + age + " years old. You can get your license.");
+		} else {
+			System.out.println("Please wait " + (16 - age) + " year(s) to get your license.");
+		}
+		
+		
+		//with else if examples
+		int temperature = 80;
+		
+		if (temperature <= 40) {
+			System.out.println("The temperature is " + temperature + ", so I will wear a heavy coat.");
+		} else if (temperature >= 60 && temperature <= 79) {
+			System.out.println("The temperature is " + temperature + ", so I will wear a light sweater.");
+		} else {
+			System.out.println("The temperature is " + temperature + ", so I will wear shorts and a tshirt.");
+		}
+		
+		//conditional statements - switch
+		char grade = 'a';
+		
+		switch (grade) {
+			case 'A':
+				System.out.println("Scored at least 90%");
+			case 'a':
+				System.out.println("Scored at least 90%");
+				break;
+			case 'B':
+				System.out.println("Scored at least 80%");
+				break;
+			case 'C':
+				System.out.println("Scored at least 70%");
+				break;
+			case 'D':
+				System.out.println("Scored at least 60%");
+				break;
+			case 'F':
+				System.out.println("Did not meet requirements");
+				
+		
+		}
+		
 		//boolean expression
 		System.out.println(2 == 2);//true
 		System.out.println(5 < 10);//true
